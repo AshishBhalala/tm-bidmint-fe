6bb3f6366b00713388010acf8a6c9834666298a8
Ashish Bhalala <ashish.b@turtlemint.com>
2021-06-25 11:44:36 +0530

    Added dash board for buyer and seller

--- src/views/buyer/buyer-dashboard/index.tsx
+++ src/views/buyer/buyer-dashboard/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Card , Modal} from 'antd';
+import { FormModel } from 'components/proposal-form';
 
 interface BuyerDashboardProps {
 	someProp: string;
 }
 
 const BuyerDashboard: React.FC<BuyerDashboardProps> = () => {
+	const [showProposalDetail, setShowProposalDetail] = useState<boolean>(false);
+
+	const viewProposalhandler = () => {
+		setShowProposalDetail(true);
+	}
+
+	const formSubmit = (formdata: any) =>{
+		console.log("values coming in buyer", formdata);	
+  }
+
+	const closeModel = () => {
+		setShowProposalDetail(false);
+	}
 	return (
 		<div>
-			<p>Buyer Dash board</p>
+			<Card style={{marginTop: 16 }} title="Card title" extra={<a href="#" onClick={viewProposalhandler} >View Proposal</a>}>
+          Content
+        </Card>
+			<Card style={{marginTop: 16 }} title="Card title" extra={<a href="#" onClick={viewProposalhandler} >View Proposal</a>}>
+          Content
+      </Card>
+      { showProposalDetail && <Modal
+					title="Proposal Detail"
+					visible={showProposalDetail}
+					width="60rem"
+					onCancel={closeModel}
+					footer={null}>
+					<FormModel formSubmit = {formSubmit}></FormModel>
+			</Modal>}
+			
 		</div>
 	);
 };
--- src/views/selller/seller-dashboard/index.tsx
+++ src/views/selller/seller-dashboard/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Card , Modal} from 'antd';
+import { FormModel } from 'components/proposal-form';
+
 
 interface SellerDashboardProps {
 	someProp: string;
 }
 
 const SellerDashboard: React.FC<SellerDashboardProps> = () => {
+	const [showProposalDetail, setShowProposalDetail] = useState<boolean>(false);
+
+	const bidNowhandler = () => {
+		setShowProposalDetail(true);
+	}
+
+	const formSubmit = (formdata: any) =>{
+		console.log("values coming in buyer", formdata);	
+  }
+
+	const closeModel = () => {
+		setShowProposalDetail(false);
+	}
 	return (
 		<div>
-			<p>Seller DashBoard</p>
+			<Card style={{marginTop: 16 }} title="Card title" extra={<a href="#" onClick={bidNowhandler} >Bid Now</a>}>
+          Content
+        </Card>
+			<Card style={{marginTop: 16 }} title="Card title" extra={<a href="#" onClick={bidNowhandler} >Bid Now</a>}>
+          Content
+      </Card>
+      { showProposalDetail && <Modal
+					title="Proposal Detail"
+					visible={showProposalDetail}
+					width="60rem"
+					onCancel={closeModel}
+					footer={null}>
+					<FormModel formSubmit = {formSubmit}></FormModel>
+			</Modal>}
+			
 		</div>
 	);
 };
